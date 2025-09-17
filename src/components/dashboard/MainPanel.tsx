0a21e3d5a034499004b80411e14696e541a10725
German Andres Gamboa <andres.gamboa.9504@gmail.com>
2025-09-16 21:37:13 -0500

    feat: ajustes en disponibilidad

--- src/components/dashboard/MainPanel.tsx
+++ src/components/dashboard/MainPanel.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import Availability from './Availability';
 
 const MainPanel: React.FC = () => (
   <div>
     <h2 className="text-xl font-bold mb-2">Panel Principal</h2>
     <p>Bienvenido al panel principal del sistema.</p>
+    <div className="mt-8">
+      <Availability />
+    </div>
   </div>
 );
 
